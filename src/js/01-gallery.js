30df9f8b437d98a4f527aa54c9546deb191a5c26
Nearh777 <nearh777@gmail.com>
2023-03-11 19:39:10 +0200

    start task 08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const basicLightbox = window.basicLightbox;
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryMarkup = hendleGalleryMarkup(galleryItems);
+
+function hendleGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+          <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}" />
+          </a>
+      </div>
+      `;
+    })
+    .join('');
+}
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+let gallery = new SimpleLightbox('.gallery a',{
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    scrollZoom: false,
+   });
+
